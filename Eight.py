e9254d54d165fcf21808de7fe33ebd1f557cb78c
Tejaspathak14 <tejaspathak1412@gmail.com>
2026-04-08 15:56:53 +0530

    Add files via upload

---
+++ Eight.py
@@ -0,0 +1,13 @@
+# Python program to print numbers from 1 to 10 using for loop and while loop
+
+# Using for loop
+print("Using for loop:")
+for i in range(1, 11):
+    print(i)
+
+# Using while loop
+print("\nUsing while loop:")
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
